aba190c09061d4a0e7dce66acb47b046839d6ea8
jayrusluy <luyjayrus03@gmail.com>
2025-10-27 02:02:47 +0800

    Add project files.

---
+++ PRMS/Form27.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form27
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs)
+        Close
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Try
+            ' Define the path to the Chrome executable
+            Dim chromePath As String = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+
+            ' Open the URL in Chrome
+            System.Diagnostics.Process.Start(chromePath, "https://drive.google.com/file/d/1blslbzG9iRNqBsd6VKge86iUlhoIRH3g/view?usp=drive_link")
+        Catch ex As Exception
+            MessageBox.Show("Unable to open the link in Chrome. Please check if Google Chrome is installed in the default path.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Form27_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ PRMS/Form3.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        DataGridView1 = New DataGridView()
+        PictureBox1 = New PictureBox()
+        Label1 = New Label()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.AllowUserToAddRows = False
+        DataGridView1.AllowUserToDeleteRows = False
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(13, 106)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.ReadOnly = True
+        DataGridView1.Size = New Size(748, 418)
+        DataGridView1.TabIndex = 0
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.DarkSeaGreen
+        PictureBox1.Location = New Point(-1, 0)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(774, 78)
+        PictureBox1.TabIndex = 1
+        PictureBox1.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.DarkSeaGreen
+        Label1.Font = New Font("Century Gothic", 21.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(75, 20)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(187, 36)
+        Label1.TabIndex = 3
+        Label1.Text = "System Logs"
+        ' 
+        ' Form3
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FloralWhite
+        ClientSize = New Size(773, 592)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox1)
+        Controls.Add(DataGridView1)
+        Name = "Form3"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "View System Logs"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+End Class
